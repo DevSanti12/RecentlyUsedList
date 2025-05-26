4f088c3aa85f15fbbd5a3a0f936b77d771ab8646
Santiago Leyva <99155189+DevSanti12@users.noreply.github.com>
2025-05-25 20:37:56 -0500

    Added Add method to RecentlyUsedList and index getter

--- RecentlyUsedListLibrary/RecentlyUsedList.cs
+++ RecentlyUsedListLibrary/RecentlyUsedList.cs
@@ -1,9 +1,22 @@
-﻿namespace RecentlyUsedListLibrary
+﻿using System.Collections.Generic;
+
+namespace RecentlyUsedListLibrary
 {
     public class RecentlyUsedList
     {
         private readonly List<string> _list = new List<string>();
 
         public int Count => _list.Count;
+
+        public void Add(string item)
+        {
+            //Insert item at the beginning lifo order
+            _list.Insert(0, item);
+        }
+
+        public string this[int index]
+        {
+            get => _list[index];
+        }
     }
 }
