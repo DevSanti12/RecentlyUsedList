fff7adf1abfb94d5f2bcbcc0780d1724d8caf869
Santiago Leyva <99155189+DevSanti12@users.noreply.github.com>
2025-05-25 20:22:37 -0500

    Added empty list unit test

--- RecentlyUsedListTests/RecentlyUsedListTests.cs
+++ RecentlyUsedListTests/RecentlyUsedListTests.cs
@@ -4,6 +4,14 @@ namespace RecentlyUsedListTests
 {
     public class RecentlyUsedListTests
     {
+        [Fact]
+        public void RecentlyUsedList_EmptyAtStart()
+        {
+            //Arrange and Act
+            var recentlyUsedList = new RecentlyUsedList();
 
+            //Assert
+            Assert.Equal(0, recentlyUsedList.Count);
+        }
     }
 }
